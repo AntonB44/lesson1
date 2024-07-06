7979bcf26d9082f8584352482b265249af223427
AntonB44 <baidakov.an44@gmail.com>
2024-07-06 08:35:25 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,9 @@
+# homework2 Практическое задание по работе в Pycharm - "Переменные"
+# Цель: научиться правильно называть переменные и взаимодействовать с ними
+Number_of_completed_ДЗ = 12
+Number_of_hours = 1.5
+Course_name = 'Python'
+Time_for_one_task = Number_of_hours / Number_of_completed_ДЗ
+print('Курс:',Course_name + ',','всего задач:',str(Number_of_completed_ДЗ) + ',','затрачено часов:',str(Number_of_hours) + ',','среднее время выполнения',Time_for_one_task,'часа.')
+
+
