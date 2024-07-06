adb9e1ca0c69fc02819845810ed1efb8693adb3f
AntonB44 <baidakov.an44@gmail.com>
2024-07-06 08:54:31 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,10 @@
+# Практическая работа по уроку "Динамическая типизация"
+# Цель: Написать программу на языке Python, используя Pycharm, для демонстрации динамической типизации.
+name = 'Антон'
+print('Name:',name)
+age = 59
+print('Age:',age)
+age = age + 1
+print('New Age:',age)
+is_student = True
+print('Is Student:',is_student)
